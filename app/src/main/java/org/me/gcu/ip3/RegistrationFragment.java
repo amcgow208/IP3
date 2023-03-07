9c506a89a635ed4c8424c5d2877bf1378b3d15f2
Andrew McGowan <amcgow208@caledonian.ac.uk>
2023-03-07 13:23:44 +0000

    Added registration page, fixed style issue with buttons
    TODO -
    
    Need to add src for logo in fragment_login
    Add Welcome page
    Create check for user credentials

---
+++ app/src/main/java/org/me/gcu/ip3/RegistrationFragment.java
@@ -0,0 +1,23 @@
+package org.me.gcu.ip3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class RegistrationFragment extends Fragment {
+
+    public RegistrationFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registration, container, false);
+    }
+}
+
