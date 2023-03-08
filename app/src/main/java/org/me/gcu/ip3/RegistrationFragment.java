2b4dcddc6ff382591cac369be3b2348bda809af7
Andrew McGowan <amcgow208@caledonian.ac.uk>
2023-03-08 12:18:20 +0000

    Back button on registration works
    TODO -
    
    Need to add src for logo in fragment_login
    Create check for user credentials
    Add functionality to signup/back buttons in registration

--- app/src/main/java/org/me/gcu/ip3/RegistrationFragment.java
+++ app/src/main/java/org/me/gcu/ip3/RegistrationFragment.java
@@ -1,23 +1,37 @@
 package org.me.gcu.ip3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class RegistrationFragment extends Fragment {
 
-    public RegistrationFragment() {
-        // Required empty public constructor
-    }
+    private Button backButton;
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_registration, container, false);
+
+        backButton = view.findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate back to LoginActivity
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
+
 
