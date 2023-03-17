75cdb92bb523097da219634ca5460c1b3fa3b087
Dreadfire <55113460+Dreadfire@users.noreply.github.com>
2023-03-17 00:30:10 +0000

    Camera is fully functional now. Ignore clutter will be cleaned up soon

--- app/src/main/java/org/me/gcu/ip3/CameraFragment.java
+++ app/src/main/java/org/me/gcu/ip3/CameraFragment.java
@@ -1,8 +1,15 @@
 package org.me.gcu.ip3;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.pm.PackageManager;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,23 +17,43 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.arch.core.util.Function;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import org.me.gcu.ip3.databinding.ActivityMainBinding;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CameraFragment extends Fragment {
 
     private static final int REQUEST_CODE_PERMISSIONS = 10;
-    private static final String[] REQUIRED_PERMISSIONS = new String[]{Manifest.permission.CAMERA};
+    private static final String[] REQUIRED_PERMISSIONS = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
     private PreviewView previewView;
+    private ExecutorService cameraExecutor;
+    private ImageCapture imageCapture;
+    //private ActivityMainBinding viewBinding;
+    //private ImageCapture imageCapture;
+    public static final String TAG = "IP3";
+    public static final String FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS";
+
+
 
     public CameraFragment() {
         // Required empty public constructor
@@ -36,7 +63,12 @@ public class CameraFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_camera, container, false);
+//        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+//        previewView = view.findViewById(R.id.previewView);
+//        return view;
+
     }
 
     @Override
@@ -50,6 +82,16 @@ public class CameraFragment extends Fragment {
         } else {
             requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
         }
+//       previewView.takePhoto.setOnClickListner(new View.OnClickListener(){
+//           @Override
+//           public void onClick(View v){
+//               takePhoto();
+//           }
+//       });
+
+        view.findViewById(R.id.image_capture_button).setOnClickListener(v -> takePhoto() );
+        cameraExecutor = Executors.newSingleThreadExecutor();
+
     }
 
     private void startCamera() {
@@ -61,12 +103,61 @@ public class CameraFragment extends Fragment {
                 try {
                     ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
                     bindPreview(cameraProvider);
+
                 } catch (ExecutionException | InterruptedException e) {
                     // Handle exceptions
                 }
             }
         }, ContextCompat.getMainExecutor(requireContext()));
     }
+    private void takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+       ImageCapture imageCapture = this.imageCapture;
+
+        if (imageCapture == null) {
+            return;
+        }
+
+        // Create time stamped name and MediaStore entry.
+        String name = new SimpleDateFormat(CameraFragment.FILENAME_FORMAT, Locale.US)
+                .format(System.currentTimeMillis());
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/Fridger-Image");
+        }
+
+        // Create output options object which contains file + metadata
+        ImageCapture.OutputFileOptions outputOptions = new ImageCapture.OutputFileOptions.Builder(
+                requireContext().getContentResolver(),
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+        )
+                .build();
+
+        // Set up image capture listener, which is triggered after photo has been taken
+        imageCapture.takePicture(
+                outputOptions,
+                ContextCompat.getMainExecutor(requireContext()),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exc) {
+                        Log.e(TAG, "Photo capture failed: " + exc.getMessage(), exc);
+                    }
+
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults output) {
+                        String msg = "Photo capture succeeded: " + output.getSavedUri();
+                        Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, msg);
+                    }
+                }
+        );
+    }
+
+
+
 
     private void bindPreview(ProcessCameraProvider cameraProvider) {
         CameraSelector cameraSelector = new CameraSelector.Builder()
@@ -78,7 +169,11 @@ public class CameraFragment extends Fragment {
 
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-        Camera camera = cameraProvider.bindToLifecycle(getViewLifecycleOwner(), cameraSelector, preview);
+        ImageCapture.Builder imageCaptureBuilder = new ImageCapture.Builder()
+                .setTargetRotation(previewView.getDisplay().getRotation());
+        imageCapture = imageCaptureBuilder.build();
+
+        Camera camera = cameraProvider.bindToLifecycle(getViewLifecycleOwner(), cameraSelector, preview, imageCapture);
     }
 
     private boolean allPermissionsGranted() {
@@ -101,4 +196,19 @@ public class CameraFragment extends Fragment {
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cameraExecutor.shutdown();
+    }
+
+
+
+
+
+
+
+
+
 }
