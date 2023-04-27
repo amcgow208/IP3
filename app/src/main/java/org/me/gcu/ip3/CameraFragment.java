d9383c689d7d1afac8ba2cd52c686daf067f3ffa
Andrew McGowan <amcgow208@caledonian.ac.uk>
2023-04-27 17:14:44 +0100

    Barcode and Food Database API connected

--- app/src/main/java/org/me/gcu/ip3/CameraFragment.java
+++ app/src/main/java/org/me/gcu/ip3/CameraFragment.java
@@ -1,14 +1,8 @@
 package org.me.gcu.ip3;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
 import android.Manifest;
-import android.content.ContentValues;
 import android.content.pm.PackageManager;
-import android.icu.text.SimpleDateFormat;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,42 +11,27 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.arch.core.util.Function;
-import androidx.camera.core.Camera;
-import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureException;
-import androidx.camera.core.Preview;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.camera.view.PreviewView;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.LifecycleOwner;
+import androidx.core.content.ContextCompat;
 
-import com.google.common.util.concurrent.ListenableFuture;
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.CompoundBarcodeView;
 
+import java.util.List;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class CameraFragment extends Fragment {
 
-    private static final int REQUEST_CODE_PERMISSIONS = 10;
-    private static final String[] REQUIRED_PERMISSIONS = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-    private PreviewView previewView;
-    private ExecutorService cameraExecutor;
-    private ImageCapture imageCapture;
-    //private ActivityMainBinding viewBinding;
-    //private ImageCapture imageCapture;
     public static final String TAG = "IP3";
-    public static final String FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS";
+    private static final int REQUEST_CODE_PERMISSIONS = 10;
+    private static final String[] REQUIRED_PERMISSIONS = new String[]{Manifest.permission.CAMERA};
 
-
+    private CompoundBarcodeView barcodeView;
 
     public CameraFragment() {
         // Required empty public constructor
@@ -61,119 +40,96 @@ public class CameraFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
         return inflater.inflate(R.layout.fragment_camera, container, false);
-//        View view = inflater.inflate(R.layout.fragment_camera, container, false);
-//        previewView = view.findViewById(R.id.previewView);
-//        return view;
-
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        previewView = view.findViewById(R.id.previewView);
-
         if (allPermissionsGranted()) {
-            startCamera();
+            setupBarcodeScanner(view);
         } else {
             requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
         }
-//       previewView.takePhoto.setOnClickListner(new View.OnClickListener(){
-//           @Override
-//           public void onClick(View v){
-//               takePhoto();
-//           }
-//       });
-
-        view.findViewById(R.id.image_capture_button).setOnClickListener(v -> takePhoto() );
-        cameraExecutor = Executors.newSingleThreadExecutor();
-
     }
 
-    private void startCamera() {
-        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext());
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (allPermissionsGranted()) {
+            barcodeView.resume();
+            setupBarcodeScanner(getView());
+        }
+    }
 
-        cameraProviderFuture.addListener(new Runnable() {
+    @Override
+    public void onPause() {
+        super.onPause();
+        barcodeView.pause();
+    }
+
+    private void setupBarcodeScanner(View view) {
+        barcodeView = view.findViewById(R.id.barcodeView);
+        barcodeView.setStatusText("");
+
+        barcodeView.decodeContinuous(new BarcodeCallback() {
             @Override
-            public void run() {
-                try {
-                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                    bindPreview(cameraProvider);
+            public void barcodeResult(BarcodeResult result) {
+                String barcodeValue = result.getText();
+                queryEdamamApi(barcodeValue);
+            }
 
-                } catch (ExecutionException | InterruptedException e) {
-                    // Handle exceptions
+            @Override
+            public void possibleResultPoints(List<ResultPoint> resultPoints) {
+            }
+        });
+    }
+
+    private void queryEdamamApi(String barcode) {
+        String appId = "6369884d";
+        String appKey = "a753cdf24eed4b05ef525a8b5d2a7d4f";
+        String baseUrl = "https://api.edamam.com/";
+
+        EdamamApiService service = RetrofitClient.getClient(baseUrl).create(EdamamApiService.class);
+        Call<FoodResponse> call = service.getFoodItemByUPC(barcode, appId, appKey);
+
+        call.enqueue(new Callback<FoodResponse>() {
+            @Override
+            public void onResponse(Call<FoodResponse> call, Response<FoodResponse> response) {
+                if (response.isSuccessful()) {
+                    FoodResponse foodResponse = response.body();
+                    if (foodResponse != null && !foodResponse.getHints().isEmpty()) {
+                        FoodItem foodItem = foodResponse.getHints().get(0).getFoodItem();
+                        if (getActivity() instanceof MainActivity) {
+                            ((MainActivity) getActivity()).onBarcodeScanned(foodItem);
+                            Log.d("API_Response", response.raw().toString());
+                        }
+                    } else {
+                        Log.e(TAG, "Error: No food item found in the response");
+                    }
+                } else {
+                    Log.e(TAG, "Error in response: " + response.errorBody());
                 }
             }
-        }, ContextCompat.getMainExecutor(requireContext()));
+
+            @Override
+            public void onFailure(Call<FoodResponse> call, Throwable t) {
+                Log.e(TAG, "Error: " + t.getMessage());
+            }
+        });
+
     }
-    private void takePhoto() {
-        // Get a stable reference of the modifiable image capture use case
-       ImageCapture imageCapture = this.imageCapture;
-
-        if (imageCapture == null) {
-            return;
-        }
-
-        // Create time stamped name and MediaStore entry.
-        String name = new SimpleDateFormat(CameraFragment.FILENAME_FORMAT, Locale.US)
-                .format(System.currentTimeMillis());
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
-        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/Fridger-Image");
-        }
-
-        // Create output options object which contains file + metadata
-        ImageCapture.OutputFileOptions outputOptions = new ImageCapture.OutputFileOptions.Builder(
-                requireContext().getContentResolver(),
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                contentValues
-        )
-                .build();
-
-        // Set up image capture listener, which is triggered after photo has been taken
-        imageCapture.takePicture(
-                outputOptions,
-                ContextCompat.getMainExecutor(requireContext()),
-                new ImageCapture.OnImageSavedCallback() {
-                    @Override
-                    public void onError(@NonNull ImageCaptureException exc) {
-                        Log.e(TAG, "Photo capture failed: " + exc.getMessage(), exc);
-                    }
-
-                    @Override
-                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults output) {
-                        String msg = "Photo capture succeeded: " + output.getSavedUri();
-                        Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show();
-                        Log.d(TAG, msg);
-                    }
-                }
-        );
+    public interface OnBarcodeScannedListener {
+        void onBarcodeScanned(FoodItem foodItem);
     }
 
+    private OnBarcodeScannedListener mListener;
 
-
-
-    private void bindPreview(ProcessCameraProvider cameraProvider) {
-        CameraSelector cameraSelector = new CameraSelector.Builder()
-                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                .build();
-
-        Preview preview = new Preview.Builder()
-                .build();
-
-        preview.setSurfaceProvider(previewView.getSurfaceProvider());
-
-        ImageCapture.Builder imageCaptureBuilder = new ImageCapture.Builder()
-                .setTargetRotation(previewView.getDisplay().getRotation());
-        imageCapture = imageCaptureBuilder.build();
-
-        Camera camera = cameraProvider.bindToLifecycle(getViewLifecycleOwner(), cameraSelector, preview, imageCapture);
+    public void setOnBarcodeScannedListener(OnBarcodeScannedListener listener) {
+        mListener = listener;
     }
+
 
     private boolean allPermissionsGranted() {
         for (String permission : REQUIRED_PERMISSIONS) {
@@ -188,26 +144,15 @@ public class CameraFragment extends Fragment {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
-                startCamera();
+                setupBarcodeScanner(getView());
             } else {
                 Toast.makeText(requireContext(), "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        cameraExecutor.shutdown();
-    }
-
-
-
-
-
-
-
-
-
 }
+
+
+
+
--- app/src/main/java/org/me/gcu/ip3/MainActivity.java
+++ app/src/main/java/org/me/gcu/ip3/MainActivity.java
@@ -1,19 +1,14 @@
 package org.me.gcu.ip3;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.arch.core.util.Function;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import org.me.gcu.ip3.LoginFragment;
-import org.me.gcu.ip3.RegistrationFragment;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraFragment.OnBarcodeScannedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +19,33 @@ public class MainActivity extends AppCompatActivity {
         loadFragment(new LoginFragment());
     }
 
-    public void loadFragment(Fragment fragment) {
+    public Fragment loadFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.fragment_container, fragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+        return fragment;
+    }
+
+    @Override
+    public void onBarcodeScanned(FoodItem foodItem) {
+        Log.d("MainActivity-FoodItem", foodItem.toString());
+        Log.d("MainActivity-Label", foodItem.getLabel());
+        Log.d("MainActivity-Nutrients", foodItem.getNutrients().toString());
+        Log.d("MainActivity-Brand", foodItem.getBrand());
+        Log.d("MainActivity-Category", foodItem.getCategory());
+        Log.d("MainActivity-FoodContents", foodItem.getFoodContentsLabel());
+        Log.d("MainActivity-Image", foodItem.getImage());
+        // Handle the navigation to FoodInfoFragment
+        FoodInfoFragment foodInfoFragment = new FoodInfoFragment();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("foodItem", foodItem);
+        foodInfoFragment.setArguments(bundle);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, foodInfoFragment)
+                .addToBackStack(null)
+                .commit();
     }
 
 }
-
-
